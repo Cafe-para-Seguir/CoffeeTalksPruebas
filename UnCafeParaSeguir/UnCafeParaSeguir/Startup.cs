e48afa1221669d55dbe62e65fe0ea117d2ceef96
Andrey Bonilla <andreybonilla23@gmail.com>
2021-03-26 17:58:26 -0600

    Pruebas Produc 2

--- UnCafeParaSeguir/UnCafeParaSeguir/Startup.cs
+++ UnCafeParaSeguir/UnCafeParaSeguir/Startup.cs
@@ -1,10 +1,13 @@
 using System;
+using System.IO;
 using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace UnCafeParaSeguir
@@ -71,6 +74,13 @@ namespace UnCafeParaSeguir
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot")),
+                RequestPath = new PathString("/wwwroot")
+            });
+
         }
     }
 }
